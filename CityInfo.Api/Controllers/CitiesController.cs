2b42cbf8446ca484040c582f80c78089e09b1076
MehrCodeLand <mehrcodeland@gmail.com>
2023-07-10 02:10:48 +0330

    Add project files.

---
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet("Api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                     new { Id=1 , Name="mehrshad" },
+                     new { Id=2 , Name="aria" }
+                });
+        }
+    }
+}
